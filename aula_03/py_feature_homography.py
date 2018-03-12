eb16047ec9912778387329f3892cf9fc72124f7b
GiuPassarelli <gb_passarelli@hotmail.com>
2018-03-12 02:07:22 -0300

    Add files via upload

--- aula_03/py_feature_homography.py
+++ aula_03/py_feature_homography.py
@@ -1,144 +1,151 @@
-#! /usr/bin/env python
-# -- coding:utf-8 --
+
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-import time
+
+# Adaptado da documentacao em http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_feature_homography/py_feature_homography.html
+# Com a funcao drawMatches do usuario
+
+def drawMatches(img1, kp1, img2, kp2, matches):
+    """
+
+    @author User rairyeng on StackOVerflow: http://stackoverflow.com/questions/20259025/module-object-has-no-attribute-drawmatches-opencv-python
+
+    My own implementation of cv2.drawMatches as OpenCV 2.4.9
+    does not have this function available but it's supported in
+    OpenCV 3.0.0
+
+    This function takes in two images with their associated
+    keypoints, as well as a list of DMatch data structure (matches)
+    that contains which keypoints matched in which images.
+
+    An image will be produced where a montage is shown with
+    the first image followed by the second image beside it.
+
+    Keypoints are delineated with circles, while lines are connected
+    between matching keypoints.
+
+    img1,img2 - Grayscale images
+    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint
+              detection algorithms
+    matches - A list of matches of corresponding keypoints through any
+              OpenCV keypoint matching algorithm
+    """
+
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+
+    print(img2)
+
+
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
+
+
+    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+
+    # Place the first image to the left
+    out[:rows1,:cols1] = np.dstack([img1, img1, img1])
+
+    # Place the next image to the right of it
+    out[:rows2,cols1:] = np.dstack([img2, img2, img2])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1,y1) = kp1[img1_idx].pt
+        (x2,y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)
+        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+
+
+    # Show the image
+    cv2.imshow('Matched Features', out)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Matched Features')
+
+    # Also return the image if you'd like a copy
+    return out
+
 
 MIN_MATCH_COUNT = 10
-detector = cv2.xfeatures2d.SIFT_create()
 
-FLANN_INDEX_KDITREE = 0
-flannParam = dict (algorithm=FLANN_INDEX_KDITREE, tree=5)
-flann = cv2.FlannBasedMatcher(flannParam,{})
+img1 = cv2.imread('box.png',0)          # Imagem a procurar
+img2 = cv2.imread('box_in_scene.png',0) # Imagem do cenario - puxe do video para fazer isto
 
-trainImg = cv2.imread("madfox.png", 0)
-trainKP,trainDesc = detector.detectAndCompute(trainImg, None)
+print(img2)
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
 
-lower = 0
-upper = 1
+# find the keypoints and descriptors with SIFT in each image
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
 
-# Returns an image containing the borders of the image
-# sigma is how far from the median we are setting the thresholds
-def auto_canny(image, sigma=0.33):
-    # compute the median of the single channel pixel intensities
-    v = np.median(image)
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
 
-    # apply automatic Canny edge detection using the computed median
-    lower = int(max(0, (1.0 - sigma) * v))
-    upper = int(min(255, (1.0 + sigma) * v))
-    edged = cv2.Canny(image, lower, upper)
+# Configura o algoritmo de casamento de features
+flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-    # return the edged image
-    return edged
+# Tenta fazer a melhor comparacao usando o algoritmo
+matches = flann.knnMatch(des1,des2,k=2)
 
-while True:
-    ret, frame = cap.read()
-
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # A gaussian blur to get rid of the noise in the image
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    #blur = gray
-    # Detect the edges present in the image
-    bordas = auto_canny(blur)
-    # Obtains a version of the edges image where we can draw in color
-    bordas_color = cv2.cvtColor(bordas, cv2.COLOR_GRAY2BGR)
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
 
 
-    '''edged = cv2.Canny(cv2.bilateralFilter(gray, 11, 17, 17), 30, 200)
-                (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-                screenCnt = None
-                for c in cnts:
-                # approximate the contour
-                    peri = cv2.arcLength(c, True)
-                    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-                    # if our approximated contour has four points, then
-                    # we can assume that we have found our screen
-                    if len(approx) == 4:
-                        screenCnt = approx
-                        break
-                cv2.drawContours(gray, [screenCnt], -1, (0, 255, 0), 3)
-                cv2.imshow("Quadrados", gray)
-                cv2.waitKey(0)'''
-
-    ret2,thresh = cv2.threshold(bordas,127,255,1)
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        if len(approx)==4:
-            cv2.drawContours(bordas_color,[cnt],0,(0,0,255),-1)
-    cv2.imshow('img',bordas_color)        
+if len(good)>MIN_MATCH_COUNT:
+    # Separa os bons matches na origem e no destino
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
 
+    # Tenta achar uma trasformacao composta de rotacao, translacao e escala que situe uma imagem na outra
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
 
-    #DETECTAR A RAPOSA:
-    queryKP, queryDesc = detector.detectAndCompute(gray,None)
-    matches = flann.knnMatch(queryDesc, trainDesc, k=2)
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
 
-    goodMatch = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            goodMatch.append(m)
-    if len(goodMatch) > MIN_MATCH_COUNT:
-        tp = []
-        qp = []
-        for m in goodMatch:
-            tp.append(trainKP[m.trainIdx].pt)
-            qp.append(queryKP[m.queryIdx].pt)
-        tp,qp = np.float32((tp,qp))
-        H,status = cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
-        h,w = trainImg.shape
-        trainBorder = np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
-        queryBorder = cv2.perspectiveTransform(trainBorder,H)
-        cv2.polylines(bordas_color,[np.int32(queryBorder)],True,(0,255,0),5)
-    else:
-        print "Not Enough match found- %d/%d" %(len(goodMatch),MIN_MATCH_COUNT)
-    cv2.imshow('result', bordas_color)
+    # Transforma os pontos da imagem origem para onde estao na imagem destino
+    dst = cv2.perspectiveTransform(pts,M)
 
+    # Desenha as linhas
+    img2b = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
 
-    #DETECTAR O CÍRCULO:
-    circles = []
+else:
+    print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
+    matchesMask = None
 
-    # HoughCircles - detects circles using the Hough Method. For an explanation of
-    # param1 and param2 please see an explanation here http://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
-    circles = None
-    circles=cv2.HoughCircles(bordas,cv2.HOUGH_GRADIENT,2,40,param1=50,param2=100,minRadius=5,maxRadius=60)
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
 
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
+img3 = drawMatches(img1,kp1,img2,kp2,good[:20])
 
-        for i in circles[0,:]:
-            print(i)
-            # draw the outer circle
-            # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]])
-            cv2.circle(bordas_color,(i[0],i[1]),i[2],(0,255,0),2)
-            # draw the center of the circle
-            cv2.circle(bordas_color,(i[0],i[1]),2,(0,0,255),3)
-
-    # Draw a diagonal blue line with thickness of 5 px
-    # cv2.line(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
-    #cv2.line(bordas_color,(0,0),(511,511),(255,0,0),5)
-
-    # cv2.rectangle(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
-    #cv2.rectangle(bordas_color,(384,0),(510,128),(0,255,0),3)
-
-    # cv2.putText(img, text, org, fontFace, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
-    #font = cv2.FONT_HERSHEY_SIMPLEX
-    #cv2.putText(bordas_color,'Ninjutsu ;)',(0,50), font, 2,(255,255,255),2,cv2.LINE_AA)
-
-    #More drawing functions @ http://docs.opencv.org/2.4/modules/core/doc/drawing_functions.htm
-
-    cv2.imshow('Detector de circulos', bordas_color)
-    #print("No circles were found")
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+plt.imshow(img3, 'gray'),plt.show()
